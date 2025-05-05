f32cc20a5323afaac181705383b97bcccf101235
Matheus Farias <matheusaf.contato@gmail.com>
2025-05-05 14:01:31 -0300

    CustomerMinDTO

--- uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
+++ uri2602/src/main/java/com/devsuperior/uri2602/Uri2602Application.java
@@ -1,5 +1,6 @@
 package com.devsuperior.uri2602;
 
+import com.devsuperior.uri2602.dto.CustomerMinDTO;
 import com.devsuperior.uri2602.projections.CustomerMinProjection;
 import com.devsuperior.uri2602.repositores.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2602Application implements CommandLineRunner {
@@ -22,9 +25,10 @@ public class Uri2602Application implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 
 		List<CustomerMinProjection> list = repository.search01("RS");
+		List<CustomerMinDTO> result1 = list.stream().map(x -> new CustomerMinDTO(x)).collect(Collectors.toList());
 
-		for(CustomerMinProjection obj : list){
-			System.out.println(obj.getName());
+		for(CustomerMinDTO obj : result1){
+			System.out.println(obj);
 		}
 	}
 }
